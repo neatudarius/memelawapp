36524018cfb5c20e9404d6fb4447f58ff7617aba
dumytru <nemtucdumitru@yahoo.com>
2014-12-13 19:44:30 +0200

    Major breakthrough

--- app/assets/javascripts/memeController.js
+++ app/assets/javascripts/memeController.js
@@ -1,43 +1,67 @@
 memeApp.controller("memeControl", ["$scope", function($scope){
 
-	$scope.currentMemes = [];
 	$scope.start = 1;
 	$scope.count = 5;
+	$scope.tags = [];
 
+	$scope.LoadTags = function(start, count){
+		$.ajax({
+		    method: "GET",
+		    url: "/tag.json?s=" + start + "c=" + count,
+		    success: function(data){
+		    	data.forEach(function(tag){
+		    		$scope.tags.push(tag.tag);
+		    	});
+		    	$("#main_search").autocomplete({
+                    source: $scope.tags
+                });
+		    }
+		});
+	}
 
 	$scope.LoadMemes = function (query){
 		$.ajax({
 		    method: "GET",
 		    url: "/query.json?q="+query+"&s="+$scope.start +"&c=" + $scope.count,
 		    success: function(data){
-		    	console.log(data);
 		    	$scope.start += $scope.count;
 				data.forEach(function(meme){
-					$scope.currentMemes.push(meme);
+					$scope.addMeme(meme);
 				});
 		    }
-		 });
-		return false;
+		});
 	}
-
-	$scope.LoadMemes('money');
 
 	$scope.RemoveNLoadMemes = function (query){
 		$scope.start = 1;
-		$scope.newCurrentMemes = [];
+		$scope.resetMeme();
 		$.ajax({
 		    method: "GET",
 		    url: "/query.json?q="+query+"&s="+$scope.start +"&c=" + $scope.count,
 		    success: function(data){
-		    	console.log(data);
 		    	$scope.start += $scope.count;
 				data.forEach(function(meme){
-					$scope.newCurrentMemes.push(meme);
+					$scope.addMeme(meme);
 				});
-				$scope.currentMemes = $scope.newCurrentMemes;
 		    }
 		 });
-		return false;
+	}
+
+	$scope.resetMeme = function(){
+		$(".memelist").empty();
+	}
+
+	$scope.addMeme = function(meme){
+		$(".memelist").append(
+			'<li class="memeitem">'
+            +'<div class="bg">'
+            +'<div class="img-thumbnail image" style="background-image: url(\''
+            +meme.url
+            +'\');"></div>'
+            +'</div>'
+            +'</li>'
+		);
 	}
 
 }]);
+
