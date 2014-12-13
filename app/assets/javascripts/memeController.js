6d31de8847669dd3dbbffcc8c10df207680bca13
dumytru <nemtucdumitru@yahoo.com>
2014-12-13 18:21:27 +0200

    dammit jim

--- app/assets/javascripts/memeController.js
+++ app/assets/javascripts/memeController.js
@@ -6,11 +6,11 @@ memeApp.controller("memeControl", ["$scope", function($scope){
 
 
 	$scope.LoadMemes = function (query){
-		$scope.start = 1;
 		$.ajax({
 		    method: "GET",
 		    url: "/query.json?q="+query+"&s="+$scope.start +"23&c=" + $scope.count,
 		    success: function(data){
+		    	console.log(data);
 		    	$scope.start += $scope.count;
 				data.forEach(function(meme){
 					//meme.url = "http://sadmoment.com/wp-content/uploads/2013/11/Give-Me-Some-Of-That-Food-Cat-Meme.jpg";
@@ -22,12 +22,15 @@ memeApp.controller("memeControl", ["$scope", function($scope){
 	}
 
 	$scope.LoadMemes('money');
+
 	$scope.RemoveNLoadMemes = function (query){
+		$scope.start = 1;
 		$scope.newCurrentMemes = [];
 		$.ajax({
 		    method: "GET",
 		    url: "/query.json?q="+query+"&s="+$scope.start +"23&c=" + $scope.count,
 		    success: function(data){
+		    	console.log(data);
 		    	$scope.start += $scope.count;
 				data.forEach(function(meme){
 					//meme.url = "http://sadmoment.com/wp-content/uploads/2013/11/Give-Me-Some-Of-That-Food-Cat-Meme.jpg";
