ce1790bb635517098988315da6ed01c0f2b1335c
Neatu Darius <neatuadarius@yahoo.com>
2014-12-13 21:56:34 +0200

    upload UpdateDb.sh

---
+++ UpdateDb.sh
@@ -0,0 +1,4 @@
+# unde from_sql.txt va fi  copy-paste la ceee ce se afiseaza in consola sql in urma comenzii
+# SELECT * FROM memes;
+# mai stergi din liniile proaste
+cat from_sql.txt | awk -F"|" '{print "INSERT INTO memes VALUES(NULL,\"" $2 "\",\"" $3 "\",0,\"" $5 "\",\"" $6 "\",0,0,0,NULL,NULL);"}' > to_append_to_MemesApp.sql.txt
